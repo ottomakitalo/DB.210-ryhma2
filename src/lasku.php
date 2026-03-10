04c667b4234742fe68304b78edfec53e0d192490
Jade Pitkanen <jade.pitkanen@tuni.fi>
2026-03-10 23:26:59 +0200

    Siirrä tarvikkeet/työtehtävät lasku tiedostoon

--- src/lasku.php
+++ src/lasku.php
@@ -11,6 +11,39 @@ if ($id === null || !array_key_exists($id, $laskut)) {
 }
 
 $lasku = $laskut[$id];
+
+$tarvikkeet = [];
+$q = pg_query($yhteys,
+    "SELECT tv.id, tv.nimi, tvt.maara, tv.yksikko, tvt.alennus
+    FROM tarvike tv
+    JOIN tarvikkeet tvt ON tvt.tarvike_id = tv.id
+    JOIN tyosuoritus ts ON ts.id = tvt.tyosuoritus_id
+    WHERE ts.id = (SELECT tyosuoritus_id FROM lasku WHERE id = $id)");
+
+while ($row = pg_fetch_assoc($q)) {
+    $tarvikkeet[$row['id']] = [
+        'tarvike' => $row['nimi'],
+        'maara'   => (float)$row['maara'],
+        'yksikko' => $row['yksikko'],
+        'alennus' => (float)$row['alennus']
+    ];
+}
+
+$tyotehtavat = [];
+$q = pg_query($yhteys,
+    "SELECT te.id, te.nimi, tet.tunnit, tet.alennus
+    FROM tyotehtava te
+    JOIN tehtavat tet ON tet.tyotehtava_id = te.id
+    JOIN tyosuoritus ts ON ts.id = tet.tyosuoritus_id
+    WHERE ts.id = (SELECT tyosuoritus_id FROM lasku WHERE id = $id)");
+
+while ($row = pg_fetch_assoc($q)) {
+    $tyotehtavat[$row['id']] = [
+        'tehtava' => $row['nimi'],
+        'tunnit'   => (float)$row['tunnit'],
+        'alennus' => (float)$row['alennus']
+    ];
+}
 ?>
 
 <!DOCTYPE html>
--- src/laskuluettelo.php
+++ src/laskuluettelo.php
@@ -28,46 +28,6 @@ while ($row = pg_fetch_assoc($q)) {
 }
 
 
-$lasku_id = $_GET['id'] ?? null;
-$valittuLasku = null;
-
-if ($lasku_id !== null && isset($laskut[$lasku_id])) {
-    $valittuLasku = $laskut[$lasku_id];
-
-    $tarvikkeet = [];
-    $q = pg_query($yhteys,
-        "SELECT tv.id, tv.nimi, tvt.maara, tv.yksikko, tvt.alennus
-        FROM tarvike tv
-        JOIN tarvikkeet tvt ON tvt.tarvike_id = tv.id
-        JOIN tyosuoritus ts ON ts.id = tvt.tyosuoritus_id
-        WHERE ts.id = (SELECT tyosuoritus_id FROM lasku WHERE id = $lasku_id)");
-
-    while ($row = pg_fetch_assoc($q)) {
-        $tarvikkeet[$row['id']] = [
-            'tarvike' => $row['nimi'],
-            'maara'   => (float)$row['maara'],
-            'yksikko' => $row['yksikko'],
-            'alennus' => (float)$row['alennus']
-        ];
-    }
-
-    $tyotehtavat = [];
-    $q = pg_query($yhteys,
-        "SELECT te.id, te.nimi, tet.tunnit, tet.alennus
-        FROM tyotehtava te
-        JOIN tehtavat tet ON tet.tyotehtava_id = te.id
-        JOIN tyosuoritus ts ON ts.id = tet.tyosuoritus_id
-        WHERE ts.id = (SELECT tyosuoritus_id FROM lasku WHERE id = $lasku_id)");
-
-    while ($row = pg_fetch_assoc($q)) {
-        $tyotehtavat[$row['id']] = [
-            'tehtava' => $row['nimi'],
-            'tunnit'   => (float)$row['tunnit'],
-            'alennus' => (float)$row['alennus']
-        ];
-    }
-}
-
 ?>
 
 <!DOCTYPE html>
