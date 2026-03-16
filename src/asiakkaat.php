0ad2668fc166707417a0359f16a1bc51b94699df
Helmi <helmi.roininen@tuni.fi>
2026-03-16 13:56:02 +0200

    työkohteen lisäys siirretty asiakas-sivulle

--- src/asiakkaat.php
+++ src/asiakkaat.php
@@ -1,7 +1,40 @@
 <?php 
 require 'db.php';
-require 'data/asiakkaat_data.php';
 require_once('navigation.php');
+
+if (isset($_POST['lisaa_tyokohde'])) {
+
+    $asiakasId = intval($_POST['asiakas_id']);
+    $osoite    = trim($_POST['osoite']);
+
+    // Generate new id for työkohde, takes the current biggest id and adds 1
+    $result = pg_query($yhteys,
+        "SELECT COALESCE(MAX(id),0)+1 AS id FROM tyokohde"
+    );
+    $row = pg_fetch_assoc($result);
+    $nextId = $row['id'];
+
+    // Insert into the database
+    
+    $update = pg_query_params(
+        $yhteys,
+        "INSERT INTO tyokohde (id, osoite, asiakas_id)
+         VALUES ($1, $2, $3)",
+        [$nextId, $osoite, $asiakasId]
+    );
+
+    //Checking if the insert succeeded
+   if (!$update || pg_affected_rows($update) === 0) {
+        die("Työkohteen lisäys epäonnistui: " . pg_last_error($yhteys));
+    }
+
+
+    // Reload page
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit;
+}
+
+require 'data/asiakkaat_data.php';
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +56,7 @@ require_once('navigation.php');
 </tr>
 
 
-<?php foreach($asiakkaat as $id => $asiakas): ?>
+<?php foreach($asiakkaat as $asiakas): ?>
 
 <tr>
     <td><?= $asiakas['nimi'] ?></td>
@@ -44,7 +77,7 @@ require_once('navigation.php');
             <?php endforeach; ?>
         </div>
         <form method="post">
-            <input type="hidden" name="asiakas_id" value="<?= $id ?>">
+            <input type="hidden" name="asiakas_id" value="<?= $asiakas['id'] ?>">
             <input type="text" name="osoite" placeholder="Osoite" required>
             <button type="submit" name="lisaa_tyokohde">Lisää työkohde</button>
         </form>
