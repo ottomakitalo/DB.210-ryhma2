aebc9254d39d35876519a4cd921b934571ea45bc
Jade Pitkänen <156824408+Jaduli@users.noreply.github.com>
2026-03-20 14:58:44 +0200

    Korjaa luontilauseet

--- SQL_luontilauseet.sql
+++ SQL_luontilauseet.sql
@@ -57,11 +57,11 @@ CREATE TABLE lasku (
   FOREIGN KEY (tyosuoritus_id) REFERENCES tyosuoritus(id) 
 );
 
-INSERT INTO lasku VALUES (1, true, '2025-10-01', '2025-10-15', null, true, 1, 1);
-INSERT INTO lasku VALUES (2, true, '2025-02-01', '2025-02-15', '2025-12-01', true, 2, 2);
-INSERT INTO lasku VALUES (3, true, '2026-02-01', '2026-02-15', null, false, 3, 3);
-INSERT INTO lasku VALUES (4, true, '2026-03-01', '2026-03-15', null, false, 2, 5);
-INSERT INTO lasku VALUES (5, true, '2026-03-01', '2026-03-15', null, false, 3, 4);
+INSERT INTO lasku VALUES (1, true, '2025-10-01', '2025-10-01', '2025-10-15', null, true, 1, 1);
+INSERT INTO lasku VALUES (2, true, '2025-02-01', '2025-02-01', '2025-02-15', '2025-12-01', true, 2, 2);
+INSERT INTO lasku VALUES (3, true, '2026-02-01', '2026-02-01', '2026-02-15', null, false, 3, 3);
+INSERT INTO lasku VALUES (4, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 2, 5);
+INSERT INTO lasku VALUES (5, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 3, 4);
 
 
 CREATE TABLE tyotehtava ( 
