0ecfa144464ab609f6b6ed2bad696cf4f6d8dd1a
Jade Pitkanen <jade.pitkanen@tuni.fi>
2026-04-01 14:37:08 +0300

    Piilota turvallisuusraportti ei-admineilta

--- src/navigation.php
+++ src/navigation.php
@@ -6,10 +6,12 @@ require_once('kayttajarooli.php');
 <div class="navigation-container content-container">
     <ul class="navigation-list">
         <li><a href="index.php" class="<?php $page === 'index.php' ? "active" : ""?>">Etusivu</a></li>
-        <?php if (isset($_SESSION['rooli']) && ($_SESSION['rooli'] === 'admin' || $_SESSION['rooli'] === 'käyttäjä')): ?>
+        <?php if (isset($_SESSION['rooli']) && ($_SESSION['rooli'] === 'käyttäjä' || $_SESSION['rooli'] === 'admin')): ?>
         <li><a href="laskut.php" class="<?php $page === 'laskut.php' ? "active" : ""?>">Laskut</a></li>
         <li><a href="asiakkaat.php" class="<?php $page === 'asiakkaat.php' ? "active" : ""?>">Asiakkaat</a></li>
         <li><a href="laskuluettelo.php" class="<?php $page === 'laskuluettelo.php' ? "active" : ""?>">Laskuluettelo</a></li>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['rooli']) && ($_SESSION['rooli'] === 'admin')): ?>
         <li><a href="turvallisuusraportti.php" class="<?php $page === 'turvallisuusraportti.php' ? "active" : ""?>">Turvallisuusraportti</a></li>
         <?php endif; ?>
     </ul>
