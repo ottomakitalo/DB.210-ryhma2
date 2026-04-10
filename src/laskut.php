d6c11dd3ea90e2f49d130b76466b213b5707b8d9
Otto Mäkitalo <otto.m@outlook.com>
2026-04-10 10:17:31 +0300

    Ohjetekstiä laskut-sivulle

--- src/laskut.php
+++ src/laskut.php
@@ -21,6 +21,8 @@ require_once('data/laskut_data.php');
 <body>
     <div class="content-container laskuluettelo">
         <h2>Laskut</h2>
+        <p>Tältä sivulta löydät kaikki laskut laskuluettelosta. Siirtyäksesi tietyn laskun tietoihin voit painaa kyseisen laskun numeroa.</p>
+        <p>Luodaksesi uuden laskun, paina alla olevaa "Siirry laskun luontiin"-nappia. Laskun luonnin sivulta voit luoda uuden laskun luomalla ensin hinta-arvion kyseiselle työlle.</p>
         <a href="laskut_hinta_arvio.php" class="link-button">Siirry laskun luontiin</a>
         <h3>Laskuluettelo</h3>
         <table>
