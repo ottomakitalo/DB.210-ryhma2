cbb4741a286a8bc27bc173df23704980f6804552
Jade Pitkänen <156824408+Jaduli@users.noreply.github.com>
2026-04-14 12:28:16 +0300

    Korjaa luontilauseet

--- SQL_luontilauseet.sql
+++ SQL_luontilauseet.sql
@@ -36,11 +36,11 @@ CREATE TABLE tyosuoritus (
   FOREIGN KEY (tyokohde_id) REFERENCES tyokohde(id) 
 ); 
 
-INSERT INTO tyosuoritus VALUES (1, 'urakka', 0, 100, 1);
+INSERT INTO tyosuoritus VALUES (1, 'urakka', 100, 0, 1);
 INSERT INTO tyosuoritus VALUES (2, 'tunti', null, null, 2);
 INSERT INTO tyosuoritus VALUES (3, 'tunti', null, null, 3);
-INSERT INTO tyosuoritus VALUES (4, 'urakka', 0, 50, 4);
-INSERT INTO tyosuoritus VALUES (5, 'tunti', null, null, 5);
+INSERT INTO tyosuoritus VALUES (4, 'urakka', 50, 0, 5);
+INSERT INTO tyosuoritus VALUES (5, 'tunti', null, null, 4);
 
 CREATE TABLE lasku ( 
   id INT PRIMARY KEY, 
@@ -60,8 +60,8 @@ CREATE TABLE lasku (
 INSERT INTO lasku VALUES (1, true, '2025-10-01', '2025-10-01', '2025-10-15', null, false, 130.20, 1, 1);
 INSERT INTO lasku VALUES (2, true, '2025-02-01', '2025-02-01', '2025-02-15', '2025-12-01', true, 716.06, 2, 2);
 INSERT INTO lasku VALUES (3, true, '2026-02-01', '2026-02-01', '2026-02-15', null, false, 2488.18, 3, 3);
-INSERT INTO lasku VALUES (4, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 74.40, 2, 5);
-INSERT INTO lasku VALUES (5, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 725.13, 3, 4);
+INSERT INTO lasku VALUES (4, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 725.13, 2, 5);
+INSERT INTO lasku VALUES (5, true, '2026-03-01', '2026-03-01', '2026-03-15', null, false, 74.40, 3, 4);
 
 CREATE TABLE tyotehtava ( 
   id INT PRIMARY KEY, 
